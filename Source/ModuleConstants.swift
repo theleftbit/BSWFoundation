1ffb015370fb47a513434d712479b0fdab2b834a
Pierluigi Cifani <piercifani@gmail.com>
2016-04-23 14:08:34 +0200

    Add undefined for better programming Create AuthStorage to store secure credentials (for now uses NSUserDefaults, it'll change to the keychain when we have more time)

--- Source/ModuleConstants.swift
+++ Source/ModuleConstants.swift
@@ -1,6 +1,6 @@
 //
-//  ModuleConstants.swift
 //  Created by Pierluigi Cifani on 20/04/16.
+//  Copyright (c) 2016 Blurred Software SL. All rights reserved.
 //
 
 import Foundation
@@ -13,4 +13,9 @@ func submoduleName(submodule : String) -> String {
 
 func queueForSubmodule(submodule : String) -> dispatch_queue_t {
     return dispatch_queue_create(submoduleName(submodule), nil)
+}
+
+public func undefined<T>(hint: String = "", file: StaticString = __FILE__, line: UInt = __LINE__) -> T {
+    let message = hint == "" ? "" : ": \(hint)"
+    fatalError("undefined \(T.self)\(message)", file:file, line:line)
 }
