a1b1f104cdd7e847054585599f99ee2e87d4f117
Pierluigi Cifani <piercifani@gmail.com>
2016-04-25 21:57:05 +0200

    Removed AuthStorage from here

--- Source/Result/Result+Deferred.swift
+++ Source/Result/Result+Deferred.swift
@@ -13,12 +13,25 @@ public func ≈> <T, U>(lhs: Future<Result<T>>, rhs: T -> Future<Result<U>>) -> 
     return lhs.flatMap { resultToDeferred($0, f: rhs) }
 }
 
+public func ≈> <T, U>(lhs: Future<Result<T>>, rhs: T -> Result<U>) -> Future<Result<U>> {
+    return lhs.flatMap { resultToDeferred($0, f: rhs) }
+}
+
 //MARK: Private
 
 private func resultToDeferred <T, U>(result: Result<T>, f: T -> Future<Result<U>>) -> Future<Result<U>> {
     switch result {
     case let .Success(value):
         return f(value)
+    case let .Failure(error):
+        return Future(value: Result.Failure(error))
+    }
+}
+
+private func resultToDeferred <T, U>(result: Result<T>, f: T -> Result<U>) -> Future<Result<U>> {
+    switch result {
+    case let .Success(value):
+        return Future(value: f(value))
     case let .Failure(error):
         return Future(value: Result.Failure(error))
     }
