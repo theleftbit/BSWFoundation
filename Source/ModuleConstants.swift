1e8ec36ac3562c632bb43e3a97955ff8d9db4783
Pierluigi Cifani <piercifani@gmail.com>
2016-04-25 15:33:10 +0200

    Remove __FILE__ for #file

--- Source/ModuleConstants.swift
+++ Source/ModuleConstants.swift
@@ -15,7 +15,7 @@ public func queueForSubmodule(submodule : String) -> dispatch_queue_t {
     return dispatch_queue_create(submoduleName(submodule), nil)
 }
 
-public func undefined<T>(hint: String = "", file: StaticString = __FILE__, line: UInt = __LINE__) -> T {
+public func undefined<T>(hint: String = "", file: StaticString = #file, line: UInt = #line) -> T {
     let message = hint == "" ? "" : ": \(hint)"
     fatalError("undefined \(T.self)\(message)", file:file, line:line)
 }
