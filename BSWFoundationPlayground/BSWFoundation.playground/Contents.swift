71d38c49630513755e7b178e943ccfec37e6a4a4
Pierluigi Cifani <piercifani@gmail.com>
2016-08-06 13:20:36 +0200

    And now Unit tests also work. One project = Playground + Tests, challenge compelted

--- BSWFoundationPlayground/BSWFoundation.playground/Contents.swift
+++ BSWFoundationPlayground/BSWFoundation.playground/Contents.swift
@@ -16,3 +16,4 @@ enum BSWEnvironment: Environment {
 }
 
 let drosky = Drosky(environment: BSWEnvironment.Production)
+print(drosky.backgroundSessionID)
--- BSWFoundationPlayground/Tests/Tests.swift
+++ BSWFoundationPlayground/Tests/Tests.swift
@@ -26,24 +26,15 @@ class Tests: XCTestCase {
             }
         }
         
-        let drosky = Drosky(environment: BSWEnvironment.Production)
+        let _ = Drosky(environment: BSWEnvironment.Production)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
