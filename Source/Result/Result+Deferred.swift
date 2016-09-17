a625cae941e6e97e10cd46daee56a6f0faac7049
Pierluigi Cifani <piercifani@gmail.com>
2016-09-17 10:42:08 +0200

    Latest functionality moved to Task<T> and TaskResult<T>

--- Source/Result/Result+Deferred.swift
+++ Source/Result/Result+Deferred.swift
@@ -29,6 +29,100 @@ public func ≈> <T, U>(lhs: Future<T>, rhs: @escaping (T) -> TaskResult<U>) -> 
     return Task(future: lhs.map(upon: DispatchQueue.any()) { return rhs($0) })
 }
 
+public func both <T, U> (first: Task<T>, second: Task<U>) ->  Task<(T, U)> {
+    
+    let deferred = Deferred<TaskResult<(T, U)>>()
+    
+    first.and(second).upon { (firstResult, secondResult) in
+        guard let firstValue = firstResult.value else {
+            deferred.fill(with: .failure(firstResult.error!))
+            return
+        }
+        
+        guard let secondValue = secondResult.value else {
+            deferred.fill(with: .failure(secondResult.error!))
+            return
+        }
+        
+        deferred.fill(with: .success((firstValue, secondValue)))
+    }
+    
+    return Task(future: Future(deferred))
+}
+
+public func bothSerially <T, U> (first: Task<T>, second: @escaping (T) -> Task<U>) ->  Task<(T, U)> {
+    
+    let deferred = Deferred<TaskResult<(T, U)>>()
+    
+    first.upon{ (firstResult) in
+        guard let firstValue = firstResult.value else {
+            deferred.fill(with: .failure(firstResult.error!))
+            return
+        }
+        
+        second(firstValue).upon { (secondResult) in
+            guard let secondValue = secondResult.value else {
+                deferred.fill(with: .failure(secondResult.error!))
+                return
+            }
+            
+            deferred.fill(with: .success((firstValue, secondValue)))
+        }
+    }
+    
+    return Task(future: Future(deferred))
+}
+
+extension Task {
+    func toObjectiveC(completionHandler handler: @escaping (SuccessValue?, NSError?) -> Void) {
+        upon(.main) { (result) in
+            switch result {
+            case .success(let value):
+                handler(value, nil)
+            case .failure(let error):
+                handler(nil, error as NSError)
+            }
+        }
+    }
+}
+
+extension TaskResult {
+    var value: Value? {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure(_):
+            return nil
+        }
+    }
+
+    var error: Error? {
+        switch self {
+        case .success(_):
+            return nil
+        case .failure(let error):
+            return error
+        }
+    }
+    
+    /// Returns a new Result by mapping `Success`es’ values using `transform`, or re-wrapping `Failure`s’ errors.
+    public func map<OtherValue>(_ transform: (Value) -> OtherValue) -> TaskResult<OtherValue> {
+        return flatMap { .success(transform($0)) }
+    }
+    
+    /// Returns the result of applying `transform` to `Success`es’ values, or re-wrapping `Failure`’s errors.
+    public func flatMap<OtherValue>(_ transform: (Value) -> TaskResult<OtherValue>) -> TaskResult<OtherValue> {
+        switch self {
+        case .failure(let error):
+            return .failure(error)
+        case .success(let value):
+            return transform(value)
+        }
+    }    
+}
+
+//MARK: Deprecated
+
 @available(*, deprecated, message: "use Task<T> instead")
 public func ≈> <T, U>(lhs: Future<Result<T>>, rhs: @escaping (T) -> Future<Result<U>>) -> Future<Result<U>> {
     return lhs.andThen(upon: DispatchQueue.any()) { resultToDeferred($0, f: rhs) }
