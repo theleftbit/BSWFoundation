d08bd27158f1ed33a275291246e9367594c00def
Pierluigi Cifani <pcifani@theleftbit.com>
2019-06-13 16:55:37 +0200

    Cleanup the Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "BSWFoundation",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "BSWFoundation",
             targets: ["BSWFoundation"]),
@@ -14,10 +13,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/bignerdranch/Deferred.git", from: "4.0.0"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "3.2.0"),
-        ],
+    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BSWFoundation",
             dependencies: ["Deferred", "KeychainAccess"]),
