94d0a6e84e81185d7c738fd724ab9236efced30c
Pierluigi Cifani <pcifani@theleftbit.com>
2019-09-01 16:35:56 +0200

    Expose the captured URLRequest in MockNetworkFetcher

--- Sources/BSWFoundation/APIClient/Mocks/MockAPIClient.swift
+++ Sources/BSWFoundation/APIClient/Mocks/MockAPIClient.swift
@@ -10,13 +10,16 @@ public class MockNetworkFetcher: APIClientNetworkFetcher {
     public init() {}
     public var mockedData: Data!
     public var mockedStatusCode: Int = 200
-    
+    public var capturedURLRequest: URLRequest?
+
     public func fetchData(with urlRequest: URLRequest) -> Task<APIClient.Response> {
+        self.capturedURLRequest = urlRequest
         let response = APIClient.Response(data: mockedData, httpResponse: HTTPURLResponse(url: urlRequest.url!, statusCode: mockedStatusCode, httpVersion: nil, headerFields: nil)!)
         return Task(success: response)
     }
     
     public func uploadFile(with urlRequest: URLRequest, fileURL: URL) -> Task<APIClient.Response> {
+        self.capturedURLRequest = urlRequest
         let response = APIClient.Response(data: mockedData, httpResponse: HTTPURLResponse(url: urlRequest.url!, statusCode: mockedStatusCode, httpVersion: nil, headerFields: nil)!)
         return Task(success: response)
     }
