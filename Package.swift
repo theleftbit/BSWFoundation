956c69377075bd618f89086729dc929593c9aa35
Pierluigi Cifani <pcifani@theleftbit.com>
2019-10-07 14:01:24 +0200

    Let's move to static and try to fix this mess

--- Package.swift
+++ Package.swift
@@ -8,19 +8,21 @@ let package = Package(
     products: [
         .library(
             name: "BSWFoundation",
-            type: .dynamic,
-            targets: ["BSWFoundation"]),
+            targets: ["BSWFoundation"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/theleftbit/Deferred.git", .branch("master")),
+        .package(url: "https://github.com/bignerdranch/Deferred.git", from: "4.1.0"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "3.2.0"),
     ],
     targets: [
         .target(
             name: "BSWFoundation",
-            dependencies: ["Deferred", "KeychainAccess"]),
+            dependencies: ["Deferred", "KeychainAccess"]
+        ),
         .testTarget(
             name: "BSWFoundationTests",
-            dependencies: ["BSWFoundation"]),
+            dependencies: ["BSWFoundation"]
+        ),
     ]
 )
