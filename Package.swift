b8ac2bfc840aff6fc97de1b90d3a8272d055717d
Pierluigi Cifani <pcifani@theleftbit.com>
2020-03-17 18:22:31 +0100

    Make this package dynamic

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     products: [
         .library(
             name: "BSWFoundation",
+            type: .dynamic,
             targets: ["BSWFoundation"]
         ),
     ],
