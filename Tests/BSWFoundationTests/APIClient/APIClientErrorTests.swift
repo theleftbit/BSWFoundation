cb0865ef6b0be048fa07c69a1faa0eeb6475cf33
Pierluigi Cifani <pcifani@theleftbit.com>
2020-04-24 10:30:07 +0200

    Improve the way APIClient.Errors are localized to the final user

---
+++ Tests/BSWFoundationTests/APIClient/APIClientErrorTests.swift
@@ -0,0 +1,18 @@
+
+import XCTest
+@testable import BSWFoundation
+
+class APIClientErrorTests: XCTestCase {
+    func testErrorPrinting_encodingRequestFailed() {
+        let localizedDescription = APIClient.Error.encodingRequestFailed.localizedDescription
+        XCTAssert(localizedDescription == "The operation couldn’t be completed. (BSWFoundation.APIClient.Error.encodingRequestFailed)")
+    }
+    
+    func testErrorPrinting_serverStatusCode() {
+        let errorMessageData =  """
+        ["Please try again"]
+        """.data(using: .utf8)
+        let localizedDescription = APIClient.Error.failureStatusCode(400, errorMessageData).localizedDescription
+        XCTAssert(localizedDescription == "The operation couldn’t be completed. (BSWFoundation.APIClient.Error.FailureStatusCode: 400, Message: [\"Please try again\"])")
+    }
+}
