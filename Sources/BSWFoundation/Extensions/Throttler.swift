3bdb1c634f8374bc81dae55e694ae55a529c194c
Pierluigi Cifani <pcifani@theleftbit.com>
2020-10-21 15:35:23 +0200

    Simplify stuff

--- Sources/BSWFoundation/Extensions/Throttler.swift
+++ Sources/BSWFoundation/Extensions/Throttler.swift
@@ -7,32 +7,20 @@ import Foundation
 
 public class Throttler {
     
-    public var queue: DispatchQueue = DispatchQueue.global(qos: .default)
-    
+    public let queue: DispatchQueue
     private var job = DispatchWorkItem(block: {})
-    private var previousRun: Date!
     private let maxInterval: Double
     
-    public init(seconds: Double) {
+    public init(seconds: Double, queue: DispatchQueue = .global(qos: .default)) {
         self.maxInterval = seconds
+        self.queue = queue
     }
     
     public func throttle(block: @escaping () -> ()) {
-        if previousRun == nil {
-            self.previousRun = Date()
-        }
         job.cancel()
-        job = DispatchWorkItem(){ [weak self] in
-            self?.previousRun = Date()
+        job = DispatchWorkItem() { 
             block()
         }
-        let delay = Date.second(from: previousRun) > maxInterval ? 0 : maxInterval
-        queue.asyncAfter(deadline: .now() + Double(delay), execute: job)
-    }
-}
-
-private extension Date {
-    static func second(from referenceDate: Date) -> Double {
-        return Date().timeIntervalSince(referenceDate).rounded()
+        queue.asyncAfter(deadline: .now() + Double(maxInterval), execute: job)
     }
 }
