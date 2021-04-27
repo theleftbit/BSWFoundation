826dc925275fa1b5b1d913e74162eef4e260e48a
Pierluigi Cifani <pcifani@theleftbit.com>
2021-04-27 11:40:35 +0200

    Last touches

--- Package.swift
+++ Package.swift
@@ -14,11 +14,12 @@ let package = Package(
     products: [
         .library(
             name: "BSWFoundation",
+            type: .dynamic,
             targets: ["BSWFoundation"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bignerdranch/Deferred.git", from: "4.1.0"),
+        .package(url: "https://github.com/theleftbit/Deferred.git", from: "4.2.0"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0"),
     ],
     targets: [
