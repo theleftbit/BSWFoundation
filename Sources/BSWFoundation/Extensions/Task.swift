cf7e1a5ae763dfe1c0305b4c016331b36bc87e3d
Pierluigi Cifani <pcifani@theleftbit.com>
2022-05-09 16:29:09 +0200

    Waiter

--- Sources/BSWFoundation/Extensions/Task.swift
+++ Sources/BSWFoundation/Extensions/Task.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 public extension Task where Success == Never, Failure == Never {
     /// Returns a Task that will never return... Well, actually, it'll complete in 1000 seconds
@@ -11,5 +12,61 @@ public extension Task where Success == Never, Failure == Never {
     static func never<T>() async throws -> T {
         let _ = try await _Concurrency.Task.never
         fatalError()
+    }
+}
+
+public extension Task where Success == Void, Failure == CancellationError {
+
+    struct ValueError: LocalizedError {
+        public var errorDescription: String? {
+            "Didn't receive asynchronous value."
+        }
+    }
+
+    static func wait<T>(operation: @escaping () async throws -> T) throws -> T {
+        var v: T? = nil
+        AsyncWaiter({
+            v = $0
+        }, operation: operation).wait()
+
+        if let v = v {
+            return v
+        } else {
+            throw ValueError()
+        }
+    }
+}
+
+/// Wait for async operation to return value and call callback with the value
+/// This class is intended to workaround/simplify async/await + actors isolation
+/// https://twitter.com/krzyzanowskim/status/1523233140914876416
+private class AsyncWaiter<T> {
+    var didReceiveValue: Bool = false
+    let value: (T) -> Void
+    let operation: () async throws -> T
+
+    init(_ value: @escaping (T) -> Void, operation: @escaping () async throws -> T) {
+        self.value = value
+        self.operation = operation
+    }
+
+    func wait() {
+        Task.detached {
+            do {
+                self.value(try await self.operation())
+                self.signal()
+            } catch {
+                self.signal()
+                throw error
+            }
+        }
+
+        while !didReceiveValue {
+            RunLoop.current.run(mode: .default, before: .distantFuture)
+        }
+    }
+
+    func signal() {
+        didReceiveValue = true
     }
 }
