66b0aa1ba8ae8c51dee30bf22632b1836117344a
Pierluigi Cifani <pcifani@theleftbit.com>
2023-06-12 12:29:27 +0200

    Move Request inside APIClient

--- Sources/BSWFoundation/APIClient/Environment.swift
+++ Sources/BSWFoundation/APIClient/Environment.swift
@@ -5,32 +5,6 @@
 
 import Foundation
 
-/// Encapsualtes the Request to be sent to the server.
-public struct Request<ResponseType>{
-
-    public typealias Validator = (APIClient.Response) throws -> ()
-    
-    /// The Endpoint where to send this request.
-    public let endpoint: Endpoint
-    
-    /// Indicates whether in case of receiving an "Unauthorized response" from the server, if it should be retried after reauthentication succeeds.
-    public let shouldRetryIfUnauthorized: Bool
-    
-    /// An optional closure to make sure any response sent by the server to this request is valid, beyond any default validation that `APIClient` makes
-    public let validator: Validator
-    
-    /// Initializes the Request
-    /// - Parameters:
-    ///   - endpoint: The Endpoint where to send this request.
-    ///   - shouldRetryIfUnauthorized: Indicates whether in case of receiving an "Unauthorized response" from the server, if it should be retried after reauthentication succeeds.
-    ///   - validator: An optional closure to make sure any response sent by the server to this request is valid, beyond any default validation that `APIClient` makes.
-    public init(endpoint: Endpoint, shouldRetryIfUnauthorized: Bool = true, validator: @escaping Validator = { _ in }) {
-        self.endpoint = endpoint
-        self.validator = validator
-        self.shouldRetryIfUnauthorized = shouldRetryIfUnauthorized
-    }
-}
-
 /// Describes an environment to attack using a `APIClient`
 public protocol Environment {
     
