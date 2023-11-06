eaef145baf8e37863a888d8638ff775ada1bd78e
michele-theleftbit <michele@theleftbit.com>
2023-11-06 11:18:23 +0100

    Make this static

--- Sources/BSWFoundation/Extensions/ProcessInfo.swift
+++ Sources/BSWFoundation/Extensions/ProcessInfo.swift
@@ -10,7 +10,7 @@ public extension ProcessInfo {
         #endif
     }
     
-    var isXcodePreview: Bool {
-        ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    static var isXcodePreview: Bool {
+        self.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
     }
 }
