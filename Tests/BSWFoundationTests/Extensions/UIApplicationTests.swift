f182b41da3302d04137ecd8779b3344a4c8db61a
Marc Hidalgo <marc@theleftbit.com>
2024-08-27 11:46:57 +0200

    More migration

--- Tests/BSWFoundationTests/Extensions/UIApplicationTests.swift
+++ Tests/BSWFoundationTests/Extensions/UIApplicationTests.swift
@@ -3,6 +3,7 @@
 
 import Testing
 import BSWFoundation
+import UIKit
 
 actor UIApplicationTests {
 
--- Tests/BSWFoundationTests/Parsing/JSONParserTests.swift
+++ Tests/BSWFoundationTests/Parsing/JSONParserTests.swift
@@ -1,18 +1,15 @@
-//
-//  Created by Pierluigi Cifani on 08/06/2017.
-//
-
-import XCTest
+import Testing
 import BSWFoundation
+import Foundation
 
-class JSONParserTests: XCTestCase {
+actor JSONParserTests {
 
     struct SampleModel: Codable {
         let identity: String
         let name: String
         let amount: Double
 
-        enum CodingKeys : String, CodingKey {
+        enum CodingKeys: String, CodingKey {
             case identity = "id", name, amount
         }
     }
@@ -29,24 +26,27 @@ class JSONParserTests: XCTestCase {
         }
     }
     
-    func testParsing_forSwiftConcurency() async throws {
+    @Test
+    func parsing_forSwiftConcurrency() async throws {
         let model = SampleModel(identity: "123456", name: "Hola", amount: 5678)
         let jsonData = try JSONEncoder().encode(model)
         let parsedModel: SampleModel = try JSONParser.parseData(jsonData)
-        XCTAssert(model.identity == parsedModel.identity)
+        #expect(model.identity == parsedModel.identity)
     }
 
-    func testParsingWithDate_forSwiftConcurency() throws {
+    @Test
+    func parsingWithDate_forSwiftConcurrency() throws {
         let sampleString = """
         {\"date\":\"2021-12-20T09:32:30+0000\"}
         """
         let model: SampleModelWithDate = try JSONParser.parseData(sampleString.data(using: .utf8)!)
-        XCTAssert(Calendar.current.component(.year, from: model.date) == 2021)
-        XCTAssert(Calendar.current.component(.month, from: model.date) == 12)
-        XCTAssert(Calendar.current.component(.day, from: model.date) == 20)
+        #expect(Calendar.current.component(.year, from: model.date) == 2021)
+        #expect(Calendar.current.component(.month, from: model.date) == 12)
+        #expect(Calendar.current.component(.day, from: model.date) == 20)
     }
     
-    func testArrayParsing() throws {
+    @Test
+    func arrayParsing() throws {
         let model1 = SampleModel(identity: "123456", name: "Hola", amount: 5678)
         let model2 = SampleModel(identity: "987642", name: "🍻", amount: 0986)
         let array = [model1, model2]
@@ -55,7 +55,8 @@ class JSONParserTests: XCTestCase {
         let _: [SampleModel] = try JSONParser.parseData(jsonData)
     }
 
-    func testParsePrettyPrinting() throws {
+    @Test
+    func parsePrettyPrinting() throws {
         let model = SampleModel(identity: "123456", name: "Hola", amount: 5678)
         let jsonData = try JSONEncoder().encode(model)
         guard let string = JSONParser.parseDataAsJSONPrettyPrint(jsonData) else {
@@ -65,10 +66,11 @@ class JSONParserTests: XCTestCase {
         let sampleString = """
         {\"amount\":5678,\"id\":\"123456\",\"name\":\"Hola\"}
         """
-        XCTAssertEqual(string, sampleString)
+        #expect(string == sampleString)
     }
 
-    func testEmptyResponseParsing() throws {
+    @Test
+    func emptyResponseParsing() throws {
         let jsonData = """
         """.data(using: .utf8)!
         let _: VoidResponse = try JSONParser.parseData(jsonData)
