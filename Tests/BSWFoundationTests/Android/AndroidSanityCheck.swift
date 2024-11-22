bc264e277f8b6f44ecbd9c2e46b0edcf46bacade
Pierluigi Cifani <pcifani@theleftbit.com>
2024-11-22 19:45:48 +0100

    Change this

--- Tests/BSWFoundationTests/Android/AndroidSanityCheck.swift
+++ Tests/BSWFoundationTests/Android/AndroidSanityCheck.swift
@@ -1,10 +1,20 @@
+#if os(Android)
 
-
-import Foundation
+import BSWFoundation
 import XCTest
 
 class SomeTest: XCTestCase {
     func testSomeStuff() {
-        XCTAssert(true)
+        let values = [0,1,2,3]
+        var array = SelectableArray<Int>(options: values)
+        array.select(atIndex: 2)
+        XCTAssert(array.selectedElement == 2)
+        array.enumerated().forEach { (offset, _) in
+            XCTAssert(array[offset] == values[offset])
+        }
+        array.appendOption(4, andSelectIt: true)
+        XCTAssert(array.selectedElement == 4)
     }
 }
+
+#endif
