335d03d88f8782cac7ea3820870e5633cfb43be5
Pierluigi Cifani <pcifani@theleftbit.com>
2024-12-10 17:09:55 +0100

    Try again

--- Sources/BSWFoundation/Extensions/NSNumber+Extensions.swift
+++ Sources/BSWFoundation/Extensions/NSNumber+Extensions.swift
@@ -2,12 +2,11 @@
 //  Created by Pierluigi Cifani on 08/05/2018.
 //
 
-#if os(Android)
-#else
 import Foundation
+#if os(Android)
+import FoundationEssentials
+#endif
 
 extension NSNumber {
     var isBool: Bool { return CFBooleanGetTypeID() == CFGetTypeID(self) }
 }
-
-#endif
