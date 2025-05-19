82a2f5b8e9f2333aee89006b3ccd7c6efa96d423
Pierluigi Cifani <pcifani@theleftbit.com>
2025-05-19 14:43:58 +0200

    More cleanup

--- Tests/BSWFoundationTests/AsyncStream/AsyncStreamDispatcherTests.swift
+++ Tests/BSWFoundationTests/AsyncStream/AsyncStreamDispatcherTests.swift
@@ -6,27 +6,10 @@ import Foundation
 import Testing
 @testable import BSWFoundation
 
-enum MyAppEvent: AsyncStreamNamedEvent, Hashable, Sendable {
-    case sendValue(value: Int)
-    case sendVoid
-    
-    var name: Name {
-        switch self {
-        case .sendValue: return .sendValue
-        case .sendVoid: return .sendVoid
-        }
-    }
-    
-    enum Name: Hashable, Sendable {
-        case sendValue
-        case sendVoid
-    }
-}
-
 // MARK: Tests
 
 struct AsyncStreamDispatcherTests {
-    
+
     @Test
     func test_sendValue_event_is_received() async throws {
         let dispatcher = AsyncStreamDispatcher<MyAppEvent>()
@@ -39,29 +22,21 @@ struct AsyncStreamDispatcherTests {
             break
         }
     }
-}
-
-// MARK: SyncBox, simple sync (non-actor) box for use with sync handlers
-
-private class SyncBox<T: Sendable>: @unchecked Sendable {
-    private var value: T
-    private let queue = DispatchQueue(label: "SyncBox", attributes: .concurrent)
-
-    init(_ value: T) {
-        self.value = value
-    }
-
-    func set(_ newValue: T) {
-        queue.async(flags: .barrier) {
-            self.value = newValue
+    
+    enum MyAppEvent: AsyncStreamNamedEvent, Hashable, Sendable {
+        case sendValue(value: Int)
+        case sendVoid
+        
+        var name: Name {
+            switch self {
+            case .sendValue: return .sendValue
+            case .sendVoid: return .sendVoid
+            }
         }
-    }
-
-    func get() -> T {
-        var result: T!
-        queue.sync {
-            result = self.value
+        
+        enum Name: Hashable, Sendable {
+            case sendValue
+            case sendVoid
         }
-        return result
     }
 }
