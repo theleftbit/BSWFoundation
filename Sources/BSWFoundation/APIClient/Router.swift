3a0732e9a79cfd001c8e5c92eafd7f6779575d03
Pierluigi Cifani <pcifani@theleftbit.com>
2025-07-08 10:58:01 +0200

    Clean this up

--- Sources/BSWFoundation/APIClient/Router.swift
+++ Sources/BSWFoundation/APIClient/Router.swift
@@ -4,8 +4,7 @@
 //
 
 #if os(Android)
-import FoundationEssentials; import FoundationInternationalization; import FoundationNetworking;
-import SkipFuse
+import FoundationEssentials; import FoundationInternationalization; import FoundationNetworking
 #endif
 import Foundation
 
@@ -30,12 +29,7 @@ extension APIClient {
 
             urlRequest.httpMethod = endpoint.method.rawValue
             urlRequest.allHTTPHeaderFields = endpoint.httpHeaderFields
-            #if os(Android)
-            let bundle = AndroidBundle.main
-            #else
-            let bundle = Bundle.main
-            #endif
-            let userAgentValue = "\(bundle.osName) - \(bundle.displayName) \(bundle.appVersion) (\(bundle.appBuild))"
+            let userAgentValue = "\(Bundle.main.osName) - \(Bundle.main.displayName) \(Bundle.main.appVersion) (\(Bundle.main.appBuild))"
             urlRequest.setValue(userAgentValue.cleanForUserAgent, forHTTPHeaderField: "User-Agent")
             if let timeout = endpoint.timeoutInterval {
                 urlRequest.timeoutInterval = timeout
--- Tests/BSWFoundationTests/APIClient/RouterTests.swift
+++ Tests/BSWFoundationTests/APIClient/RouterTests.swift
@@ -9,6 +9,14 @@ import Foundation
 actor RouterTests {
 
     @Test
+    func userAgentGeneration() async throws {
+        let sut = APIClient.Router(environment: Giphy.Hosts.production)
+        let urlRequest = try await sut.urlRequest(forEndpoint: Giphy.API.search("hola"))
+        let userAgent = try #require(urlRequest.allHTTPHeaderFields?["User-Agent"])
+        #expect(userAgent.contains(Bundle.main.osName))
+    }
+    
+    @Test
     func simpleURLEncoding() async throws {
         let sut = APIClient.Router(environment: Giphy.Hosts.production)
         let urlRequest = try await sut.urlRequest(forEndpoint: Giphy.API.search("hola"))
