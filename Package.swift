cba8841c65e688f9e36fa6e3d4bff0bdffe6feef
Pierluigi Cifani <pcifani@theleftbit.com>
2026-03-14 13:49:59 +0100

    Bump this

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ var packageDependencies: [Package.Dependency] = [
 if skipIsEnabled {
     packageDependencies.append(contentsOf: [
         .package(url: "https://source.skip.tools/skip-fuse.git", from: "1.0.2"),
-        .package(url: "https://source.skip.tools/skip-keychain.git", from: "0.3.0"),
+        .package(url: "https://source.skip.tools/skip-keychain.git", from: "0.3.2"),
     ])
 }
 
